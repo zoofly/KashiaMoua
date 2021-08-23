ac1377863fcf4d6e5db679e7ad89ea6f90a2990f
Kash <68801820+zoofly@users.noreply.github.com>
2021-08-23 00:56:34 -0500

    Delete extra-work directory

--- extra-work/fibonacci-sequence.js
+++
@@ -1,9 +0,0 @@
-function sequence (){
-    let numberArray = [1,1];
-    while (numberArray.length< 200){
-    let val = numberArray[numberArray.length-1]+numberArray[numberArray.length-2];
-    numberArray.push(val);
-    }
-    return numberArray
-    
-}
